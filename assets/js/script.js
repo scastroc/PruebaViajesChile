65d0bf55d5e19cd5588e784d97e87fd093038954
Sebastian Castro Contreras <sebastian.a.castroc@gmail.com>
2022-01-23 00:20:46 -0300

    Commit secciones faltantes y scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
-$('.carousel').carousel({
+$(document).ready(function() {
+  $('.carousel').carousel({
     interval: 8000,
     pause: "false"
   });
+});
+
+$(document).scroll( function() {
+  const y = $("html").scrollTop();
+    if(y > 700) $('.navbar').addClass("bg-black")
+    else $('.navbar').removeClass("bg-black")
+});
